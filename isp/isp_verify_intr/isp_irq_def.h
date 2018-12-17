f94c5e2af4b9e46c7040d3b57c5f547d8051d0fe
doujianglong <doujianglong@pinecone.net>
2018-12-17 21:01:28 +0800

    v1: isp: add verify isp intr funcion
    Change-Id: Id14300300489191fc439968d2b1682f9b924521c

---
+++ isp/isp_verify_intr/isp_irq_def.h
@@ -0,0 +1,74 @@
+/****************************************************************************
+ * apps/external/isp/isp_verify_intr/isp_irq_def.h
+ *
+ *   Copyright (C) 2008, 2011-2012 Gregory Nutt. All rights reserved.
+ *   Author: Pinecone <pinecone@pinecone.net>
+
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef ISP_IRQ_DEF
+#define ISP_IRQ_DEF
+
+#define ISP1_IRQ 0
+#define ISP2_IRQ 1
+#define ISP3_IRQ 2
+#define ISP4_IRQ 3
+
+#define ISP1_BASE 0xFA450000
+#define ISP2_BASE 0xFA451000
+#define ISP3_BASE 0xFA452000
+#define ISP4_BASE 0xFA453000
+
+#define INT_SOF_EOF_EN	0xa00
+#define ING_SOF_EOF		0xa04
+	#define BIT_EOF		1
+	#define BIT_SOF		0
+
+#define REG_ISP_HEIGHT	0x08
+#define REG_ISP_WIDTH	0x0c
+#define REG_AWB_STAT_WIDTH	0xe0
+#define REG_AWB_STAT_HEIGHT	0xe4
+#define REG_AE_STAT_WIDTH	0x1b0
+#define REG_AE_STAT_HEIGHT	0x1b4
+#define REG_PORT0_HEIGHT	0x810
+#define REG_PORT0_WIDTH		0x814
+#define REG_PORT1_HEIGHT	0x828
+#define REG_PORT1_WIDTH		0x82c
+#define REG_PORT2_HEIGHT	0x840
+#define REG_PORT2_WIDTH		0x844
+#define REG_PORT3_HEIGHT	0x858
+#define REG_PORT3_WIDTH		0x85c
+#define REG_RD_PORT_HEIGHT	0x908
+#define REG_RD_PORT_WIDTH	0x90c
+#define REG_RD_PORT_VB		0x910
+#define REG_RD_PORT_HB		0x914
+
+#endif
---
+++ isp/isp_verify_intr/isp_verify_intr_main.c
@@ -0,0 +1,188 @@
+/****************************************************************************
+ * apps/external/isp/isp_hello/isp_hello_main.c
+ *
+ *   Copyright (C) 2008, 2011-2012 Gregory Nutt. All rights reserved.
+ *   Author: Pinecone <pinecone@pinecone.net>
+
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+#include "nuttx/irq.h"
+#include "isp_irq_def.h"
+
+#define setreg32(addr, val)		(*(volatile uint32_t *)(addr) = (val))
+#define getreg32(addr)			(*(volatile uint32_t *)(addr))
+#define setreg16(addr, val)		(*(volatile uint16_t *)(addr) = (val))
+#define getreg16(addr)			(*(volatile uint16_t *)(addr))
+#define setreg8(addr, val)		(*(volatile uint8_t *)(addr) = (val))
+#define getreg8(addr)			(*(volatile uint8_t *)(addr))
+
+#define set_bit(n, bit, x)	(n = (n & ~(1 << bit)) | ((x << bit)))
+
+unsigned int g_isp1_irq;
+
+int isp_isr(uint32_t irq, void *data, void *arg)
+{
+	int addr, val;
+	{
+		addr = ISP1_BASE + ING_SOF_EOF;
+		val = getreg32(addr);
+		g_isp1_irq |= val;
+		setreg32(addr, val & 0x3);
+	}
+	return 0;
+}
+static void mapping_irqs()
+{
+	irq_attach(ISP1_IRQ, isp_isr, NULL);
+	up_enable_irq(ISP1_IRQ);
+}
+
+static int init_irqs()
+{
+	mapping_irqs();
+	setreg32(ISP1_BASE + INT_SOF_EOF_EN, ((1 << BIT_EOF) | (1 << BIT_SOF)));
+	return 0;
+}
+
+static int start_single_stream()
+{
+	unsigned int *y_addr = 0x80000000;
+	unsigned int *u_addr = 0x80200000;
+	unsigned int *v_addr = 0x80400000;
+
+	setreg32(ISP1_BASE + REG_ISP_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_ISP_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_AWB_STAT_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_AWB_STAT_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_AE_STAT_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_AE_STAT_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_PORT0_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_PORT0_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_PORT1_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_PORT1_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_PORT2_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_PORT2_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_PORT3_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_PORT3_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_RD_PORT_WIDTH, 176);
+	setreg32(ISP1_BASE + REG_RD_PORT_HEIGHT, 144);
+	setreg32(ISP1_BASE + REG_RD_PORT_VB, 0x100);
+
+	setreg32(0xfa450010, 0x08);
+	setreg32(0xfa450800, 0x07);
+	setreg32(0xfa450860, y_addr);
+	setreg32(0xfa450864, u_addr);
+	setreg32(0xfa450868, v_addr);
+
+	return 0;
+
+}
+
+void isp_irq_listener_thread(void *arg)
+{
+	while (1) {
+		if (g_isp1_irq & 0x2) {
+			syslog(LOG_INFO, "get eof!\n");
+			set_bit(g_isp1_irq, 1, 0);
+		}
+		if (g_isp1_irq & 0x1) {
+			syslog(LOG_INFO, "get sof!\n");
+			set_bit(g_isp1_irq, 0, 0);
+		}
+
+		usleep(1);
+	}
+
+}
+
+void isp_main_loop(void *arg)
+{
+	while (1) {
+		usleep(1);
+	}
+
+}
+
+static int verify_intr_task(int argc, char *argv[])
+{
+	init_irqs();
+	start_single_stream();
+
+	pthread_t isp_mainloop_thread;
+	pthread_attr_t isp_mainloop_attr;
+	struct sched_param isp_mainloop_param;
+	pthread_attr_init(&isp_mainloop_attr);
+	isp_mainloop_param.sched_priority = SCHED_PRIORITY_DEFAULT;
+	pthread_attr_setschedparam(&isp_mainloop_attr, &isp_mainloop_param);
+	pthread_attr_setstacksize(&isp_mainloop_attr, 2048);
+	pthread_create(&isp_mainloop_thread, &isp_mainloop_attr, isp_main_loop, NULL);
+
+	pthread_t isp_irq_thread;
+	pthread_attr_t isp_irq_attr;
+	struct sched_param isp_irq_param;
+	pthread_attr_init(&isp_irq_attr);
+	isp_irq_param.sched_priority = SCHED_PRIORITY_DEFAULT;
+	pthread_attr_setschedparam(&isp_irq_attr, &isp_irq_param);
+	pthread_attr_setstacksize(&isp_irq_attr, 2048);
+	pthread_create(&isp_irq_thread, &isp_irq_attr, isp_irq_listener_thread, NULL);
+
+
+	return 0;
+}
+
+/****************************************************************************
+ * isp_hello_main
+ ****************************************************************************/
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int isp_verify_intr_main(int argc, char *argv[])
+#endif
+{
+	int ret =0;
+
+	ret = task_create(argv[0],
+			CONFIG_ISP_VERIFY_INTR_PRIORITY,
+			CONFIG_ISP_VERIFY_INTR_STACKSIZE,
+			verify_intr_task,
+			argv + 1);
+
+	return ret > 0 ? 0 : ret;
+}

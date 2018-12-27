5765ff236dadb352a959950a675bcb8c7a3a31b6
doujianglong <doujianglong@pinecone.net>
2018-12-27 17:36:31 +0800

    v1: isp: add a fake simple host running on riscv
    Change-Id: I4bd8c30998298d75f930bf170a646b99133fcd54

---
+++ isp/isp_simple_host/defines.h
@@ -0,0 +1,167 @@
+/****************************************************************************
+ * apps/external/isp/isp_simple_host/defines.h
+ *
+ *   Copyright (C) 2008, 2011-2012 Gregory Nutt. All rights reserved.
+ *   Author: Pinecone <pinecone@pinecone.net>
+
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef ISP_DEFINES
+#define ISP_DEFINES
+
+#define PIPE_NUM	4
+#define PORT_NUM	4
+
+#define FW_CMD_BUFFER_BASE	0x1b000
+#define FW_ISP_SETTING_BASE	0x1c000
+
+#define INT_ISPCTL_EN	0xfa3e0404
+#define INT_ISPCTL_BIT_EN	0xfa3e0408
+#define INT_ISPCTL		0xfa3e040c
+#define REG_ISPCTL_TRIGGER	0xfa3e0400
+	#define ISPCTL_INIT_FW	0
+	#define ISPCTL_SETFMT	1
+	#define ISPCTL_BRACKET_CAP	2
+	#define ISPCTL_3	3
+	#define ISPCTL_4	4
+	#define ISPCTL_5	5
+	#define ISPCTL_6	6
+	#define ISPCTL_7	7
+	#define ISPCTL_INIT_FW_DONE	8
+	#define ISPCTL_SETFMT_DONE	9
+	#define ISPCTL_BRACKET_CAP_DONE	10
+	#define ISPCTL_11	11
+	#define ISPCTL_12	12
+	#define ISPCTL_13	13
+	#define ISPCTL_14	14
+	#define ISPCTL_15	15
+
+#define ISP1_BASE 0xFA450000
+#define ISP2_BASE 0xFA451000
+#define ISP3_BASE 0xFA452000
+#define ISP4_BASE 0xFA453000
+	#define ISP_BASE_OFFSET	0x1000
+
+#define INT_SOF_EOF_EN	0xa00
+#define INT_SOF_EOF		0xa04
+	#define BIT_EOF		1
+	#define BIT_SOF		0
+#define INT_STAT_DONE_EN	0xa18
+#define INT_STAT_DONE		0xa1c
+	#define BIT_AE_DONE		1
+	#define BIT_AWB_DONE	0
+
+#ifdef CONFIG_ISP_SIMPLE_HOST
+	#define INT_AXI_START_EN    0xa08
+	#define INT_AXI_START       0xa0c
+	#define INT_AXI_END_EN      0xa10
+	#define INT_AXI_END         0xa14
+#endif
+
+#define REG_SOFT_RSTN	0x00
+#define REG_ISP_HEIGHT	0x08
+#define REG_ISP_WIDTH	0x0c
+#define REG_ISP_COLORBAR	0x10
+#define REG_AWB_STAT_WIDTH	0xe0
+#define REG_AWB_STAT_HEIGHT	0xe4
+#define REG_AE_STAT_WIDTH	0x1b0
+#define REG_AE_STAT_HEIGHT	0x1b4
+#define REG_PORT0_CTRL		0x800
+#define REG_PORT0_HEIGHT	0x810
+#define REG_PORT0_WIDTH		0x814
+#define REG_PORT1_CTRL		0x800
+#define REG_PORT1_HEIGHT	0x828
+#define REG_PORT1_WIDTH		0x82c
+#define REG_PORT2_CTRL		0x800
+#define REG_PORT2_HEIGHT	0x840
+#define REG_PORT2_WIDTH		0x844
+#define REG_PORT3_CTRL		0x800
+#define REG_PORT3_HEIGHT	0x858
+#define REG_PORT3_WIDTH		0x85c
+	#define PORT_OFFSET		0x18
+#define REG_RD_PORT_HEIGHT	0x908
+#define REG_RD_PORT_WIDTH	0x90c
+#define REG_RD_PORT_VB		0x910
+#define REG_RD_PORT_HB		0x914
+
+/* operations */
+#define setreg32(addr, val)		(*(volatile uint32_t *)(addr) = (uint32_t)(val))
+#define getreg32(addr)			(*(volatile uint32_t *)(addr))
+#define setreg16(addr, val)		(*(volatile uint16_t *)(addr) = (uint16_t)(val))
+#define getreg16(addr)			(*(volatile uint16_t *)(addr))
+#define setreg8(addr, val)		(*(volatile uint8_t *)(addr) = (uint8_t)(val))
+#define getreg8(addr)			(*(volatile uint8_t *)(addr))
+
+static inline void setreg32_mask(uint32_t addr, uint32_t val, uint32_t mask)
+{
+	uint32_t v = getreg32(addr);
+	v = (v & ~mask) | (val & mask);
+	setreg32(addr, v);
+}
+
+static inline void setreg16_mask(uint32_t addr, uint16_t val, uint16_t mask)
+{
+	uint16_t v = getreg16(addr);
+	v = (v & ~mask) | (val & mask);
+	setreg16(addr, v);
+}
+
+static inline void setreg8_mask(uint32_t addr, uint8_t val, uint8_t mask)
+{
+	uint8_t v = getreg8(addr);
+	v = (v & ~mask) | (val & mask);
+	setreg8(addr, v);
+}
+
+#define set_bit(n, bit, x)	(n = (n & ~(1 << bit)) | ((x << bit)))
+
+typedef enum {
+	SRC_SENSOR = 0,
+	SRC_MEM,
+	SRC_COLORBAR
+} input_t;
+
+typedef struct {
+	bool port_en;
+	uint16_t width;
+	uint16_t height;
+} port_fmt_t;
+
+typedef struct {
+	uint8_t pipe_id;
+	input_t input_type;
+	bool str_on_off_flag;
+	uint16_t pipe_width;
+	uint16_t pipe_height;
+	port_fmt_t port_fmt[PORT_NUM];
+} stream_fmt_t;
+
+#endif
---
+++ isp/isp_simple_host/isp_simple_host_main.c
@@ -0,0 +1,234 @@
+/****************************************************************************
+ * apps/external/isp/isp_simple_host/isp_simple_host_main.c
+ *
+ *   Copyright (C) 2008, 2011-2012 Gregory Nutt. All rights reserved.
+ *   Author: Pinecone <pinecone@pinecone.net>
+
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <semaphore.h>
+#include <memory.h>
+
+#include "nuttx/irq.h"
+#include "nuttx/arch.h"
+#include "defines.h"
+
+unsigned int g_ispctl_irq;
+/* reserve 108 ~ 112k for cmd buffer */
+stream_fmt_t *g_fmt = NULL;
+
+#define ISPCTL_IRQ 27
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
+sem_t sem_cmd;
+
+int initialize_fw(void)
+{
+	/* enable ispctl irqs */
+	setreg32(INT_ISPCTL_EN, 0x1);
+	setreg32(INT_ISPCTL_BIT_EN, 0xffff);
+
+	setreg32(REG_ISPCTL_TRIGGER, ISPCTL_INIT_FW);
+	pthread_mutex_lock(&lock);
+	pthread_cond_wait(&cond, &lock);
+	pthread_mutex_unlock(&lock);
+	syslog(LOG_INFO, "%s done\n", __func__);
+
+	return 0;
+}
+
+static void load_isp_setting(void)
+{
+	setreg32(0xfa4500c0, 0x05); // enable awb and awb stat
+	setreg32(0xfa4501a0, 0x03); // enable ae and ae stat
+	setreg32(0xfa4510c0, 0x05);
+	setreg32(0xfa4511a0, 0x03);
+	setreg32(0xfa4520c0, 0x05);
+	setreg32(0xfa4521a0, 0x03);
+	setreg32(0xfa4530c0, 0x05);
+	setreg32(0xfa4531a0, 0x03);
+
+	setreg32(0xfa4500e0, 176);
+	setreg32(0xfa4500e4, 100); // smaller than height(144)
+	setreg32(0xfa4501b0, 176);
+	setreg32(0xfa4501b4, 100); // smaller than height(144)
+
+	setreg32(0xfa4510e0, 176);
+	setreg32(0xfa4510e4, 100); // smaller than height(144)
+	setreg32(0xfa4511b0, 176);
+	setreg32(0xfa4511b4, 100); // smaller than height(144)
+
+	setreg32(0xfa4520e0, 176);
+	setreg32(0xfa4520e4, 100); // smaller than height(144)
+	setreg32(0xfa4521b0, 176);
+	setreg32(0xfa4521b4, 100); // smaller than height(144)
+
+	setreg32(0xfa4540e0, 176);
+	setreg32(0xfa4540e4, 100); // smaller than height(144)
+	setreg32(0xfa4541b0, 176);
+	setreg32(0xfa4541b4, 100); // smaller than height(144)
+
+}
+
+int start_single_stream(void)
+{
+	if (g_fmt == NULL) {
+		g_fmt = (stream_fmt_t *)FW_CMD_BUFFER_BASE;
+	}
+
+	memset(g_fmt, 0, sizeof(stream_fmt_t));
+	g_fmt->pipe_id = 0;
+	g_fmt->input_type = SRC_COLORBAR;
+	g_fmt->str_on_off_flag = true;
+	g_fmt->pipe_width = 176;
+	g_fmt->pipe_height = 144;
+	g_fmt->port_fmt[0].port_en = true;
+	g_fmt->port_fmt[0].width = 176;
+	g_fmt->port_fmt[0].height =144;
+	g_fmt->port_fmt[1].port_en = true;
+	g_fmt->port_fmt[1].width = 176;
+	g_fmt->port_fmt[1].height =144;
+
+
+	setreg32(REG_ISPCTL_TRIGGER, ISPCTL_SETFMT);
+	pthread_mutex_lock(&lock);
+	pthread_cond_wait(&cond, &lock);
+	pthread_mutex_unlock(&lock);
+
+	g_fmt = NULL;
+
+	unsigned int *y_addr = (uint32_t *)0x80000000;
+	unsigned int *u_addr = (uint32_t *)0x80200000;
+	unsigned int *v_addr = (uint32_t *)0x80400000;
+
+	setreg32(0xfa450860, y_addr);
+	setreg32(0xfa450864, u_addr);
+	setreg32(0xfa450868, v_addr);
+
+	return 0;
+
+}
+
+void *ispctl_listener(void *arg)
+{
+	while (1) {
+		sem_wait(&sem_cmd);
+		if (g_ispctl_irq & (1 << ISPCTL_INIT_FW_DONE)) {
+			syslog(LOG_INFO, "get ispctl done, g_ispctl_irq = 0x%x\n", g_ispctl_irq);
+			g_ispctl_irq = 0;
+			pthread_mutex_lock(&lock);
+			pthread_cond_signal(&cond);
+			pthread_mutex_unlock(&lock);
+		}
+
+		if (g_ispctl_irq & (1 << ISPCTL_SETFMT_DONE)) {
+			syslog(LOG_INFO, "get set format done, g_ispctl_irq = 0x%x\n", g_ispctl_irq);
+			g_ispctl_irq = 0;
+			pthread_mutex_lock(&lock);
+			pthread_cond_signal(&cond);
+			pthread_mutex_unlock(&lock);
+		}
+	}
+}
+
+int host_ispctl_isr(int irq, void *data, void *arg)
+{
+	int addr, val;
+	addr = INT_ISPCTL;
+	val = getreg32(addr);
+	g_ispctl_irq |= val;
+	setreg32(addr, val);
+
+	sem_post(&sem_cmd);
+
+	return 0;
+}
+
+int host_init_irqs(void)
+{
+	irq_attach(ISPCTL_IRQ, host_ispctl_isr, NULL);
+	up_enable_irq(ISPCTL_IRQ);
+
+	return 0;
+}
+static int simple_host_task(int argc, char *argv[])
+{
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	sem_init(&sem_cmd, 0, 0);
+
+	pthread_t ispctl_listener_thread;
+	pthread_attr_t ispctl_listener_attr;
+	struct sched_param ispctl_listener_param;
+	pthread_attr_init(&ispctl_listener_attr);
+	ispctl_listener_param.sched_priority = SCHED_PRIORITY_DEFAULT;
+	pthread_attr_setschedparam(&ispctl_listener_attr, &ispctl_listener_param);
+	pthread_attr_setstacksize(&ispctl_listener_attr, 2048);
+	pthread_create(&ispctl_listener_thread, &ispctl_listener_attr, ispctl_listener, NULL);
+
+	host_init_irqs();
+	initialize_fw();
+	load_isp_setting();
+	start_single_stream();
+
+	return 0;
+}
+
+/****************************************************************************
+ * isp_simple_host
+ ****************************************************************************/
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int isp_simple_host_main(int argc, char *argv[])
+#endif
+{
+	int ret =0;
+
+	ret = task_create(argv[0],
+			CONFIG_ISP_SIMPLE_HOST_PRIORITY,
+			CONFIG_ISP_SIMPLE_HOST_STACKSIZE,
+			simple_host_task,
+			argv + 1);
+
+	return ret > 0 ? 0 : ret;
+}

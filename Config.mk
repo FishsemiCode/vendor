26b3c7023ff5b09ed3ce89f0ea964227920a9f3f
ZhongAn <zhongan@pinecone.net>
2019-03-19 14:13:17 +0800

    tmwvt: export toshiba voice trigger middleware func.
    Change-Id: I6e980b2a16607b8ec371332a75240206ccdd217a
    Signed-off-by: ZhongAn <zhongan@pinecone.net>

--- Config.mk
+++ Config.mk
@@ -96,6 +96,10 @@ ifneq ($(CONFIG_CODEC_FDKAAC),)
     EXXFLAGS += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" $(APPDIR)/external/system/codec/fdk-aac/libPCMutils/include}
 endif
 
+ifneq ($(CONFIG_CODEC_TMWVT),)
+    EFLAGS += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" $(APPDIR)/external/system/codec/tmwvt}
+endif
+
 ifneq ($(CONFIG_CODEC_BLUEDROID),)
     EFLAGS += ${shell $(INCDIR) $(INCDIROPT) "$(CC)" $(APPDIR)/external/system/codec/bluedroid/decoder/include}
 endif
